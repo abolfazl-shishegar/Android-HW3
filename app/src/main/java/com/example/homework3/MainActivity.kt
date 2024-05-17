e96ece5d93e9a0a3599006fc1680b50997cea06e
Abolfazl Shishegar <104524823+abolfazl-shishegar@users.noreply.github.com>
2024-05-17 16:46:36 +0330

    Update MainActivity.kt

--- app/src/main/java/com/example/homework3/MainActivity.kt
+++ app/src/main/java/com/example/homework3/MainActivity.kt
@@ -9,18 +9,23 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
@@ -30,26 +35,67 @@ import java.io.File
 import java.io.FileWriter
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : ComponentActivity() {
     private lateinit var networkChangeReceiver: BroadcastReceiver
     private val isConnected = mutableStateOf(true)
+    private val connectionLogs = mutableStateListOf<JSONObject>()
 
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        networkChangeReceiver = NetworkChangeReceiver()
-        IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION).also {
-            registerReceiver(networkChangeReceiver, it, Context.RECEIVER_NOT_EXPORTED)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            registerNetworkCallback()
+        } else {
+            networkChangeReceiver = NetworkChangeReceiver()
+            IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION).also {
+                registerReceiver(networkChangeReceiver, it)
+            }
         }
+
         setContent {
-            NetworkStatusDisplay(isConnected.value)
+            NetworkStatusScreen(isConnected.value, connectionLogs)
         }
+        checkInitialNetworkState()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        unregisterReceiver(networkChangeReceiver)
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            unregisterReceiver(networkChangeReceiver)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun registerNetworkCallback() {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkRequest = NetworkRequest.Builder()
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            .build()
+        connectivityManager.registerNetworkCallback(networkRequest, object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                isConnected.value = true
+                showNotification(isConnected.value)
+                logConnectionStatus(isConnected.value)
+            }
+
+            override fun onLost(network: Network) {
+                isConnected.value = false
+                showNotification(isConnected.value)
+                logConnectionStatus(isConnected.value)
+            }
+        })
+    }
+
+    private fun checkInitialNetworkState() {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = connectivityManager.activeNetwork
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork)
+        isConnected.value = networkCapabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) == true
+        showNotification(isConnected.value)
+        logConnectionStatus(isConnected.value)
     }
 
     inner class NetworkChangeReceiver : BroadcastReceiver() {
@@ -79,13 +125,6 @@ class MainActivity : ComponentActivity() {
                 Manifest.permission.POST_NOTIFICATIONS
             ) != PackageManager.PERMISSION_GRANTED
         ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return
         }
         NotificationManagerCompat.from(this).notify(1, notification)
@@ -104,6 +143,7 @@ class MainActivity : ComponentActivity() {
             notificationManager.createNotificationChannel(channel)
         }
     }
+
     private fun logConnectionStatus(isConnected: Boolean) {
         val connectionStatus = if (isConnected) "Connected" else "Disconnected"
         val statusLog = JSONObject().apply {
@@ -112,23 +152,31 @@ class MainActivity : ComponentActivity() {
             put("Status", connectionStatus)
         }
 
-        val publicDocsDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
-        val logsFile = File(publicDocsDir, "connection_logs.json")
-        if (!logsFile.exists()) {
-            logsFile.createNewFile()
+        val logsFile = File(applicationContext.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), "connection_logs.json")
+        val logs = if (logsFile.exists() && logsFile.length() != 0L) {
+            JSONArray(logsFile.readText())
+        } else {
+            JSONArray()
         }
-        val logs = if (logsFile.length() == 0L) JSONArray() else JSONArray(logsFile.readText())
         logs.put(statusLog)
         FileWriter(logsFile, false).use { it.write(logs.toString()) }
+
+        connectionLogs.add(0, statusLog)
     }
 
 
 }
 
 @Composable
-fun NetworkStatusDisplay(isConnected: Boolean) {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(text = if (isConnected) "Connected to Internet" else "Disconnected")
+fun NetworkStatusScreen(isConnected: Boolean, connectionLogs: List<JSONObject>) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = if (isConnected) "Connected to Internet" else "Disconnected")
+        }
     }
 }
-
